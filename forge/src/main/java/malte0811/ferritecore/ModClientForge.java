cd58db849c5c9ea583c4387ed9aadb268053b295
malte0811 <malte0811@web.de>
2021-01-30 17:53:19 +0100

    Clean up gradle setup

--- forge/src/main/java/malte0811/ferritecore/ModClientForge.java
+++ forge/src/main/java/malte0811/ferritecore/ModClientForge.java
@@ -3,10 +3,12 @@ package malte0811.ferritecore;
 import malte0811.ferritecore.impl.Deduplicator;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ParticleFactoryRegisterEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(value = Dist.CLIENT, modid = ModMain.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModClientForge {
+    @SubscribeEvent
     public static void registerReloadListener(ParticleFactoryRegisterEvent ev) {
         Deduplicator.registerReloadListener();
     }
