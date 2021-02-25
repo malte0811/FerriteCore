043fc8d07f8105fee1b97843e4ab963d3f12b807
malte0811 <malte0811@web.de>
2021-02-25 10:49:38 +0100

    Forge: Do not warn when only present on one side

--- forge/src/main/java/malte0811/ferritecore/ModMainForge.java
+++ forge/src/main/java/malte0811/ferritecore/ModMainForge.java
@@ -2,8 +2,12 @@ package malte0811.ferritecore;
 
 import cpw.mods.modlauncher.api.INameMappingService;
 import malte0811.ferritecore.util.Constants;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 
 @Mod(Constants.MODID)
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
@@ -12,6 +16,10 @@ public class ModMainForge {
     public ModMainForge() {
         Constants.blockstateCacheFieldName = ObfuscationReflectionHelper.remapName(
                 INameMappingService.Domain.FIELD, "field_215707_c"
+        );
+        ModLoadingContext.get().registerExtensionPoint(
+                ExtensionPoint.DISPLAYTEST,
+                () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (s, b) -> true)
         );
     }
 }
