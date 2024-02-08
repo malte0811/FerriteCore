37c615fbb16eea694d685f81b5f37947f2d500e8
malte0811 <malte0811@web.de>
2024-02-08 16:47:36 +0100

    Initial update to 1.20.5

--- NeoForge/src/main/java/malte0811/ferritecore/ModClientForge.java
+++ NeoForge/src/main/java/malte0811/ferritecore/ModClientForge.java
@@ -4,10 +4,11 @@ import malte0811.ferritecore.impl.Deduplicator;
 import malte0811.ferritecore.util.Constants;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.event.RenderLevelStageEvent;
 
-@Mod.EventBusSubscriber(value = Dist.CLIENT, modid = Constants.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(value = Dist.CLIENT, modid = Constants.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class ModClientForge {
     @SubscribeEvent
     public static void registerReloadListener(RenderLevelStageEvent.RegisterStageEvent ev) {
--- NeoForge/src/main/java/malte0811/ferritecore/ModMainForge.java
+++ NeoForge/src/main/java/malte0811/ferritecore/ModMainForge.java
@@ -4,14 +4,7 @@ import malte0811.ferritecore.util.Constants;
 import net.neoforged.fml.IExtensionPoint;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.network.NetworkConstants;
 
 @Mod(Constants.MODID)
 public class ModMainForge {
-
-    public ModMainForge() {
-        ModLoadingContext.get().registerExtensionPoint(
-                IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (s, b) -> true)
-        );
-    }
 }
